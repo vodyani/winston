b89ac18b45eb112f18f9bf9e585e29cf29e57447
John Denver <chogathkk@gmail.com>
2022-04-14 15:33:35 +0800

    style: code

--- src/base/logger.ts
+++ src/base/logger.ts
@@ -43,10 +43,7 @@ export class BaseLogger implements LoggerService {
 
   @FixedContext
   public error(error: Error, source: string, data?: any) {
-    const { name, stack, message } = error;
-    const exception = { message, name, stack };
-
-    this.instance.error({ source: this.getSource(source), data, exception });
+    this.instance.error({ source: this.getSource(source), data, ...error });
   }
 
   @FixedContext
