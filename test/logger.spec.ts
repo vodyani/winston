4298e212d0782ba76621027e45fda4a758785365
John Denver <chogathkk@gmail.com>
2022-08-10 14:23:25 +0800

    chore(jest): write unit tests and validate the results 🌈

--- test/logger.spec.ts
+++ test/logger.spec.ts
@@ -6,26 +6,72 @@ import { LoggerFactory } from '../src';
 
 describe('LoggerFactory', () => {
   const factory = new LoggerFactory();
+  const error = new Error('I\'m Error 😏');
 
-  it('simple', async () => {
+  it('empty logger', async () => {
     const logger = factory.create({
       env: 'LOCAL',
-      name: 'simple-logger',
-      mode: ['Console', 'DailyRotateFile'],
-      handleExceptions: true,
-      handleRejections: true,
-      transportOptions: {
-        dirname: resolve(__dirname, './temp'),
-      },
+      name: 'empty-logger',
+      handleExceptions: false,
+      handleRejections: false,
+      mode: [],
     });
 
-    const error = new Error('I\'m Error 😏');
+    logger.log('log input');
+  });
+
+  it('logger', async () => {
+    const logger = factory.create({
+      env: 'LOCAL',
+      name: 'logger',
+      handleExceptions: true,
+      handleRejections: true,
+      mode: ['Console', 'File', 'DailyRotateFile'],
+      fileOptions: {
+        dirname: resolve(__dirname, './temp/file'),
+      },
+      dailyRotateFileOptions: {
+        dirname: resolve(__dirname, './temp/daily-rotate-file'),
+      },
+    });
 
     logger.log('log input');
     logger.info('info input');
     logger.warn('warn input');
-    logger.debug({ tag: 'From Test LoggerFactory' });
-    logger.error(error, { tag: 'From Test LoggerFactory' });
-    logger.error(new Error('No extra Error'));
+    logger.debug('debug input');
+    logger.error(error, 'error input');
+    logger.error(error);
+  });
+
+  it('custom logger filename', async () => {
+    const logger = factory.create({
+      env: 'LOCAL',
+      name: 'custom-logger',
+      handleExceptions: true,
+      handleRejections: true,
+      mode: ['File', 'DailyRotateFile'],
+      fileOptions: {
+        createFilename: (key: string) => `custom-logger-${key}.log`,
+      },
+      dailyRotateFileOptions: {
+        createFilename: (key: string) => `%DATE%-custom-logger-${key}.log`,
+      },
+    });
+
+    logger.log('log input');
+    logger.error(error, 'log input');
+  });
+
+  it('empty options logger', async () => {
+    const logger = factory.create({
+      env: 'LOCAL',
+      name: 'custom-options-logger',
+      handleExceptions: true,
+      handleRejections: true,
+      mode: ['File', 'DailyRotateFile'],
+    });
+
+    logger.log('log input');
+    logger.error(error, 'log input');
   });
 });
